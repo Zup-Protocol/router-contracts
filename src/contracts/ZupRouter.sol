e5f975445cd525963c9a6053c9948c14b3769235
Ryan Holanda <ryanholanda08@gmail.com>
2024-09-10 13:55:05 -0300

    refactor: update imports to use relational imports

--- src/contracts/ZupRouter.sol
+++ src/contracts/ZupRouter.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.26;
 
-import {IZupRouter, PoolToken} from "src/interfaces/IZupRouter.sol";
+import {IZupRouter, PoolToken} from "../interfaces/IZupRouter.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {FeeController} from "src/contracts/FeeController.sol";
+import {FeeController} from "./FeeController.sol";
 
 //
 //                                                   XEX
